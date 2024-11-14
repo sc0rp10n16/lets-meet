35769fc6efdfe14397fb9e96b615cf856c0a3961
Akshith Mysa <akshithmysa@outlook.com>
2024-11-14 20:35:11 +0530

    day1

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar";
+
+export default function DashboardHome() {
+  return (
+    <section className="">
+        <div>
+          <Navbar />
+        </div>
+    </section>
+    
+  );
+}
+
